09d3c4e132a29e77ed852d805154f3035f3fa980
Danny Tan <dannytan11@gmail.com>
2021-10-28 16:21:57 +1100

    set up api

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -1,1 +1,16 @@
+import RequestService from 'services/RequestService';
+import config from 'config';
+console.log(config)
 
+const requestService = new RequestService(config.api.baseUrl);
+
+export async function searchMovieTitle(title) {
+  const url = `/?s=${title}&type=movie&r=json&apikey=${config.api.key}`;
+  try {
+    const response = await requestService.get(url);
+
+    return response;
+  } catch (error) {
+    return Promise.reject(error.response.data.message);
+  }
+}
--- src/config/config.local.js
+++ src/config/config.local.js
@@ -1,2 +1,7 @@
 module.exports = {
+  api: {
+    env: process.env.REACT_APP_NODE_ENV,
+    baseUrl: 'https://cors-anywhere.herokuapp.com/http://www.omdbapi.com',
+    key: process.env.REACT_APP_OMDB_API_KEY,
+  }
 };
